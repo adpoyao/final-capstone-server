20d12d721ae02d14adc5ff1a431d8be478c8f851
Adam Bradbury <f.adambrad@gmail.com>
2018-01-19 14:25:27 -0700

    get endpoint retrieves a class from db. Get endpoint test still passes

--- class/router.js
+++ class/router.js
@@ -1,25 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { Class } = require('./models');
 const router = express.Router();
 const jsonParser = bodyParser.json();
 
 
 router.get('/', (req, res) => {
-
-    res.json([{'className': 'BIO103'}]);
+  return Class.find()
+    .then(data => res.json(data.map(data => data.apiRepr())))
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
   });
 
 
 router.post('/', jsonParser, (req, res) => {
-
-    const item = {'id':'123', 'firstName':'John', 'lastName':'Doe', 'username':'username', 'className':'BIO103'}
+  
+    const item = {
+    'id':'123', 
+    'firstName':'John', 
+    'lastName':'Doe', 
+    'username':'username', 
+    'className':'BIO103'
+  }
     res.status(201).json(item);
 });
 
 
 router.put('/:id', jsonParser, (req, res) => {
-    const updatedItem = {'id':'123', 'firstName':'John', 'lastName':'Doe', 'className':'BIO103'}
+    const updatedItem = {
+      'id':'123', 
+      'firstName':'John', 
+      'lastName':'Doe', 
+      'className':'BIO103'
+    }
     res.status(200).json(updatedItem);
   });
 
